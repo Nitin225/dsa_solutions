ccb745cfcc58eba531bbb2fbdf8fd0c622204d9b
Nitin <177527946+Nitin225@users.noreply.github.com>
2026-03-25 00:22:47 +0530

    changes in tilling_problem1.cpp

--- tilling_problem1.cpp
+++ tilling_problem1.cpp
@@ -1,9 +1,25 @@
 // time = O(2^n)
 // space = O(n)
-// count total no of ways tile a floor (2*n) with tile (2*1)
+// Description: count total no of ways tile a floor (2*n) with tile (2*1)
+
+#include <iostream>
+using namespace std;
 
 int numTilling(int n) {
-  if(n == 0) return 1;
-  if(n == 1) return 1;
-  return numTilling(n-1) + numTilling(n-2);
+    if(n == 0) return 1;
+    if(n == 1) return 1;
+
+    return numTilling(n-1) + numTilling(n-2);
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    int ways = numTilling(n);
+
+    cout << "Total ways: " << ways << endl;
+
+    return 0;
 }
